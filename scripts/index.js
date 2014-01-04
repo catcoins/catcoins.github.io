c5d87992065f1c4e19c39c7b3ea31e95fddfdc56
Charles Feng <charles@charles.io>
2014-01-04 15:40:26 -0800

    remove info about fork

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,6 @@ window.onload = function () {
 
 var update = function () {
   updatePrice();
-  updateFork();
 };
 
 var getRequest = function () {
@@ -28,23 +27,6 @@ var updatePrice = function () {
     request.onreadystatechange = function () {
       if (request.readyState == 4) {
         document.getElementById('trade').innerHTML = request.responseText;
-      }
-    };
-  } catch (err) {}
-};
-
-var updateFork = function () {
-  try {
-    var request = getRequest();
-    request.open('GET', 'http://catchain.info/chain/Catcoin/q/getblockcount', true);
-    request.send(null);
-    request.onreadystatechange = function () {
-      if (request.readyState == 4 && request.responseText && !isNaN(request.responseText)) {
-        var blocks = Math.ceil(20290 - parseInt(request.responseText, 10), 0);
-        if (!blocks) { return; }
-        document.getElementById('fork-blocks').innerHTML = ' in ' + blocks + ' block' + (blocks === 1 ? '' : 's');
-        var mins = blocks * 90, hours = Math.floor(mins / 60); mins = mins - hours * 60;
-        document.getElementById('fork-time').innerHTML = 'in ~' + hours + ' hour' + (hours === 1 ? '' : 's') + ', ' + mins + ' minute' + (mins === 1 ? '' : 's') + ' ';
       }
     };
   } catch (err) {}
